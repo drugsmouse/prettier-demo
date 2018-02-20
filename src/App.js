527a4331eedc86d22259171400f0d73cc6072a63
Dmytro Tereshchenko <dmytro_tereshchenko@epam.com>
2018-02-20 17:02:36 +0200

    test precommit hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const emxampleForThePrecommitHook = {
+  some: 'object',
+  without: 'trailingComma'
+}
+
 class App extends Component {
   render() {
     return (
